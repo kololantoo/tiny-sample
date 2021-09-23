7134b390b0a2661c8d18bbee47eca0fc8c19fb4e
zhengyang <zhengyang1@eastmoney.com>
2021-09-23 14:04:42 +0800

    项目初始化

---
+++ src/main/java/com/kololantoo/tinysample/TinySampleApplication.java
@@ -0,0 +1,18 @@
+package com.kololantoo.tinysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: zhengyang
+ * @version: 1.0
+ * @date: 2021/9/23
+ * @description:
+ */
+@SpringBootApplication
+public class TinySampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TinySampleApplication.class, args);
+    }
+
+}
